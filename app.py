87e2d7888acbe325192f82f9e9c236f2665e6c83
BuildTools <unconfigured@null.spigotmc.org>
2023-12-07 16:35:13 +0000

    Barebones

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import streamlit as st
 
+def user_input():
+    name = st.text_input(label='Name')
+    uploaded_files = st.file_uploader(label='Files', accept_multiple_files=True, type=["pdf"])
+    note = st.text_input(label='Note')
+    return name, uploaded_files, note
+
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Faidz Printing",
+        page_icon="🖨️",
     )
+    
+    name, uploaded_files, note = user_input()
 
-    st.write("# Welcome to Streamlit! 👋")
 
+run()
